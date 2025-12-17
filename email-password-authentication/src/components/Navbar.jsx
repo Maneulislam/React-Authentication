29631f840c4a7c1a2d86165a48cf2399a7e06be7
Tonmoy <tonmoy@Tonmoy.Tonmoy>
2025-12-17 12:43:45 +0600

    Complete

--- email-password-authentication/src/components/Navbar.jsx
+++ email-password-authentication/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router';
+import { Link, NavLink } from 'react-router';
 
 const Navbar = () => {
 
@@ -38,7 +38,7 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <Link to='/register' className="btn">Register</Link>
             </div>
         </div>
     );
